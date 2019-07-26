d35204ebcc3d35d0ad9920c022185b30268fa909
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-07-26 10:26:29 -0300

    dependency(vue-toasted): add vue-toasted

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router';
 import store from './store';
 import './registerServiceWorker';
 import vuetify from './plugins/vuetify';
+import './plugins/toasted';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import '@mdi/font/css/materialdesignicons.css';
 
