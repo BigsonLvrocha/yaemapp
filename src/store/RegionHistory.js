d3b8f282088ac4494d4d444153e34d3f419e9f27
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-07-30 12:09:52 -0300

    feat(RegionHistoryStore): add data digestion

--- src/store/RegionHistory.js
+++ src/store/RegionHistory.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import EsiService from '@/services/api';
 import * as AsyncMixins from './mixin/Async';
+import { getItemHistoryDetail } from '@/util/ItemHistory';
 
 export default {
   namespaced: true,
@@ -44,8 +45,7 @@ export default {
         const types = response.data.slice();
         const totalPages = Number.parseInt(response.headers['x-pages'], 10);
         if (!totalPages || totalPages < 2) {
-          commit('SET_REGION_TYPES', { regionId, types });
-          return;
+          return types;
         }
         const pages = _.range(2, totalPages + 1);
         const requests = pages.map(page => EsiService.get(`/markets/${regionId}/types`, {
@@ -54,13 +54,56 @@ export default {
           },
         }));
         const responses = await Promise.all(requests);
-        commit('SET_REGION_TYPES', {
-          regionId,
-          types: types.concat(...responses.map(resp => resp.data)),
+        return types.concat(...responses.map(resp => resp.data));
+      } catch (error) {
+        commit('ADD_ERROR', { error }, { root: true });
+        throw error;
+      } finally {
+        commit('CLEAR_IS_LOADING');
+      }
+    },
+    async fetchRegionHistoryData({ dispatch, commit }, { regionId }) {
+      try {
+        commit('SET_IS_LOADING');
+        const types = await dispatch('fetchRegionHistoryTypes', { regionId });
+        if (types.length === 0) {
+          return;
+        }
+        const regionTypesHistoryData = [];
+        const totalFetches = Math.floor((types.length - 1) / 200) + 1;
+        for (let i = 0; i < totalFetches; i += 1) {
+          const roundTypes = types.slice(i * 200, ((i + 1) * 200));
+          const requests = roundTypes.map(typeId => dispatch('fetchItemHistoryData', {
+            regionId, typeId,
+          }));
+          // eslint-disable-next-line no-await-in-loop
+          const results = await Promise.all(requests);
+          regionTypesHistoryData.push(...results);
+        }
+        commit('SET_REGION_TYPES', { types: regionTypesHistoryData.filter(item => item !== null && item.hist7.averageIskVolume > 1000000), regionId });
+      } catch (error) {
+        commit('ADD_ERROR', { error }, { root: true });
+        throw error;
+      } finally {
+        commit('CLEAR_IS_LOADING');
+      }
+    },
+    async fetchItemHistoryData({ commit }, { regionId, typeId }) {
+      try {
+        commit('SET_IS_LOADING');
+        const { data } = await EsiService.get(`/markets/${regionId}/history`, {
+          params: {
+            type_id: typeId,
+          },
         });
-      } catch (err) {
-        commit('ADD_ERROR', err, { root: true });
-        throw err;
+        const details = getItemHistoryDetail(data);
+        return {
+          typeId,
+          ...details,
+        };
+      } catch (error) {
+        commit('ADD_ERROR', { error }, { root: true });
+        return null;
       } finally {
         commit('CLEAR_IS_LOADING');
       }
