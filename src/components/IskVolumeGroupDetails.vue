025dd180378c1830d45da060eaa0e3b555702253
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-08-01 01:21:11 -0300

    feat(IskVolumeGroupDetails): add data overview per item

--- src/components/IskVolumeGroupDetails.vue
+++ src/components/IskVolumeGroupDetails.vue
@@ -1,16 +1,71 @@
 <template>
-  <v-list>
-    <v-list-item
-      v-for="(type, index) in regionData.types"
-      :key="type.typeId"
-    >
-      <v-list-item-title>
-        {{ typesInfo[index] === undefined
-          ? 'Loading...'
-          : `${typesInfo[index].name} (${typesInfo[index].type_id})` }}
-      </v-list-item-title>
-    </v-list-item>
-  </v-list>
+  <v-data-table
+    :items="typesInfo"
+    :headers="headers"
+    :items-per-page="5"
+    sort-by="hist30.averageIskVolume"
+    sort-desc
+    item-key="type_id"
+    must-sort
+  >
+    <template #item.type.name="{ item, value }">
+      {{ value === undefined ? 'loading' : value }}
+    </template>
+    <template #item.hist30.averageIskVolume="{ item, value }">
+      {{ (value / 1000000).toLocaleString('default', {
+        maximumFractionDigits: 2
+      }) }} M ± {{
+        (item.hist30.stdDevIskVolume / value).toLocaleString('default', {
+          style: 'percent'
+        })
+      }}
+    </template>
+    <template #item.hist15.averageIskVolume="{ item, value }">
+      {{ (value / 1000000).toLocaleString('default', {
+        maximumFractionDigits: 2
+      }) }} M ± {{
+        (item.hist15.stdDevIskVolume / value).toLocaleString('default', {
+          style: 'percent'
+        })
+      }}
+    </template>
+    <template #item.hist7.averageIskVolume="{ item, value }">
+      {{ (value / 1000000).toLocaleString('default', {
+        maximumFractionDigits: 2
+      }) }} M ± {{
+        (item.hist7.stdDevIskVolume / value).toLocaleString('default', {
+          style: 'percent'
+        })
+      }}
+    </template>
+    <template #item.hist30.averageVolume="{ item, value }">
+      {{ (value).toLocaleString('default', {
+        maximumFractionDigits: 0
+      }) }} ± {{
+        (item.hist30.stdDevVolume / value).toLocaleString('default', {
+          style: 'percent'
+        })
+      }}
+    </template>
+    <template #item.hist15.averageVolume="{ item, value }">
+      {{ (value).toLocaleString('default', {
+        maximumFractionDigits: 0
+      }) }} ± {{
+        (item.hist15.stdDevVolume / value).toLocaleString('default', {
+          style: 'percent'
+        })
+      }}
+    </template>
+    <template #item.hist7.averageVolume="{ item, value }">
+      {{ (value).toLocaleString('default', {
+        maximumFractionDigits: 0
+      }) }} ± {{
+        (item.hist7.stdDevVolume / value).toLocaleString('default', {
+          style: 'percent'
+        })
+      }}
+    </template>
+  </v-data-table>
 </template>
 
 <script>
@@ -23,20 +78,69 @@ export default {
       type: Object,
     },
   },
+  data() {
+    return {
+      headers: [
+        {
+          value: 'type.name',
+          text: 'Name',
+          sortable: true,
+        },
+        {
+          value: 'hist7.averageIskVolume',
+          text: 'Average Isk Volume (7 days)',
+          sortable: true,
+        },
+        {
+          value: 'hist7.averageVolume',
+          text: 'Average Volume (7 days)',
+          sortable: true,
+        },
+        {
+          value: 'hist15.averageIskVolume',
+          text: 'Average Isk Volume (15 days)',
+          sortable: true,
+        },
+        {
+          value: 'hist15.averageVolume',
+          text: 'Average Volume (15 days)',
+          sortable: true,
+        },
+        {
+          value: 'hist30.averageIskVolume',
+          text: 'Average Isk Volume (30 days)',
+          sortable: true,
+        },
+        {
+          value: 'hist30.averageVolume',
+          text: 'Average Volume (30 days)',
+          sortable: true,
+        },
+      ],
+      pagination: {
+        orderBy: 'hist30.averageIskVolume',
+        descending: true,
+        rowsPerPage: 5,
+      },
+    };
+  },
   computed: {
     typesInfo() {
       return this.regionData.types.map(
-        item => this.$store.state.Types.data.find(a => a.type_id === item.typeId),
+        item => ({
+          ...item,
+          type: this.$store.state.Types.data.find(a => a.type_id === item.typeId),
+        }),
       );
     },
-  },
-  mounted() {
-    this.fetchTypeIdArrayData({ typeIds: this.regionData.types.map(item => item.typeId) });
   },
   methods: {
     ...mapActions('Types', [
       'fetchTypeIdArrayData',
     ]),
+    tablePaged(items) {
+      this.fetchTypeIdArrayData({ typeIds: items.map(item => item.typeId) });
+    },
   },
 };
 </script>
--- src/util/ItemHistory.js
+++ src/util/ItemHistory.js
@@ -4,17 +4,18 @@ export function getItemPeriodDetail(history, period) {
   const threshold = subDays(Date.now(), period);
   const validDates = history.filter(item => isAfter(new Date(item.date), threshold));
   const averagePrice = validDates.reduce((prev, curr) => (prev + curr.average), 0) / period;
-  const averagePriceVariation = validDates.reduce((prev, curr) => (
-    prev + (curr.highest - curr.lowest)
-  ), 0);
+  const priceVariation = validDates.map(item => (item.highest - item.lowest) / item.average);
+  const averagePriceVariation = priceVariation.reduce((prev, curr) => (
+    prev + curr
+  ), 0) / (period);
   const averageVolume = validDates.reduce((prev, curr) => (prev + curr.volume), 0) / period;
   const stdDevVolume = Math.sqrt(validDates.reduce((prev, curr) => (
-    prev + (curr.volume - averageVolume) * (curr.volume - averageVolume)
+    prev + ((curr.volume - averageVolume) ** 2)
   ), 0) / (period - 1));
   const iskVolume = validDates.map(item => item.volume * item.average);
   const averageIskVolume = iskVolume.reduce((prev, curr) => (prev + curr), 0) / period;
   const stdDevIskVolume = Math.sqrt(iskVolume.reduce((prev, curr) => (
-    prev + (curr - averageIskVolume) * (curr - averageIskVolume)
+    prev + ((curr - averageIskVolume) ** 2)
   ), 0) / (period - 1));
   return {
     averagePrice,
