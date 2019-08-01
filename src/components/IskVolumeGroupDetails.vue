b0ee11c8df503088f8b0e2b8a59538c56d41d1ce
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-08-01 16:08:49 -0300

    feat(IskVolumeGroupDetails): add margin display

--- src/components/IskVolumeGroupDetails.vue
+++ src/components/IskVolumeGroupDetails.vue
@@ -1,6 +1,6 @@
 <template>
   <v-data-table
-    :items="typesInfo"
+    :items="typesInfoWithMargin"
     :headers="headers"
     :items-per-page="5"
     @current-items="tablePaged"
@@ -91,11 +91,25 @@
         maximumSignificantDigits: 4,
       }) }}
     </template>
+    <template #item.margin="{value}">
+      <span v-if="!isStationSelected">Select station!</span>
+      <v-progress-circular
+        v-else-if="value === undefined"
+        indeterminate
+      />
+      <span v-else>
+        {{
+          value.margin.toLocaleString()
+        }} ({{
+          value.marginPct.toLocaleString('default', { style: 'percent' })
+        }})
+      </span>
+    </template>
   </v-data-table>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapState } from 'vuex';
 
 export default {
   props: {
@@ -157,15 +171,28 @@ export default {
           text: 'Avg. price var. (30 d)',
           sortable: true,
         },
+        {
+          value: 'margin',
+          text: 'Price margin',
+          sortable: false,
+        },
       ],
-      pagination: {
-        orderBy: 'hist30.averageIskVolume',
-        descending: true,
-        rowsPerPage: 5,
-      },
     };
   },
   computed: {
+    ...mapState('StationSelect', [
+      'regionId',
+      'stationId',
+    ]),
+    ...mapState('Types', {
+      loadingTypes: 'idsLoading',
+    }),
+    ...mapState('OrdersMargin', {
+      loadingMargins: 'idsLoading',
+    }),
+    isStationSelected() {
+      return !!this.regionId && !!this.stationId;
+    },
     typesInfo() {
       return this.regionData.types.map(
         item => ({
@@ -174,13 +201,52 @@ export default {
         }),
       );
     },
+    typesInfoWithMargin() {
+      if (!this.isStationSelected) {
+        return this.typesInfo;
+      }
+      return this.typesInfo.map(
+        item => ({
+          ...item,
+          margin: this.$store.state.OrdersMargin.data.find(
+            a => a.typeId === item.typeId && a.stationId === this.stationId,
+          ),
+        }),
+      );
+    },
   },
   methods: {
     ...mapActions('Types', [
       'fetchTypeIdArrayData',
+
+    ]),
+    ...mapActions('OrdersMargin', [
+      'fetchMarginsData',
+      'setMarginsToLoad',
     ]),
     tablePaged(items) {
-      return this.fetchTypeIdArrayData({ typeIds: items.map(item => item.typeId) });
+      const untyped = items
+        .filter(item => item.type === undefined)
+        .map(item => item.typeId);
+      const noMargin = items
+        .filter(item => item.margin === undefined)
+        .map(item => item.typeId);
+      if (untyped.length > 0) {
+        this.fetchTypeIdArrayData({
+          typeIds: untyped,
+        });
+      }
+      if (noMargin.length > 0) {
+        if (this.isStationSelected) {
+          this.fetchMarginsData({
+            typeIds: noMargin,
+            stationId: this.stationId,
+            regionId: this.regionId,
+          });
+        } else {
+          this.setMarginsToLoad({ typeIds: noMargin });
+        }
+      }
     },
   },
 };
