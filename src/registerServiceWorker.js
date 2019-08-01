bdddcd9c56b2393e0efba00853fb057a3e21f155
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-08-01 12:20:34 -0300

    feat(ServiceWorker): add toasted info display

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 
 import { register } from 'register-service-worker';
+import Vue from 'vue';
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
@@ -9,24 +10,32 @@ if (process.env.NODE_ENV === 'production') {
         'App is being served from cache by a service worker.\n'
         + 'For more details, visit https://goo.gl/AFskqB',
       );
+      Vue.toasted.info('App is being served from cache by a service worker.\n'
+      + 'For more details, visit https://goo.gl/AFskqB');
     },
     registered() {
       console.log('Service worker has been registered.');
+      Vue.toasted.info('Service worker has been registered.');
     },
     cached() {
       console.log('Content has been cached for offline use.');
+      Vue.toasted.info('Content has been cached for offline use.');
     },
     updatefound() {
       console.log('New content is downloading.');
+      Vue.toasted.info('New content is downloading.');
     },
     updated() {
       console.log('New content is available; please refresh.');
+      Vue.toasted.info('New content is available; please refresh.');
     },
     offline() {
       console.log('No internet connection found. App is running in offline mode.');
+      Vue.toasted.info('No internet connection found. App is running in offline mode.');
     },
     error(error) {
       console.error('Error during service worker registration:', error);
+      Vue.toasted.info('Error during service worker registration:');
     },
   });
 }
